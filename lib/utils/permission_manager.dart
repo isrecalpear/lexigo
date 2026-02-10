66f0aa9c674910cbc2564e48cce4a9aebc8a4287
isrecalpear <64584752+isrecalpear@users.noreply.github.com>
2026-02-10 23:12:36 +0800

    bug fix: macOS与iOS上无需检查文件读写权限

--- lib/utils/permission_manager.dart
+++ lib/utils/permission_manager.dart
@@ -1,8 +1,16 @@
+// Dart imports:
+import 'dart:io';
+
 // Package imports:
 import 'package:permission_handler/permission_handler.dart';
 
 class PermissionManager {
   static Future<void> _ensureExternalIOAccess() async {
+    if (Platform.isMacOS || Platform.isIOS) {
+      // On iOS and macOS, we don't need to request storage permissions
+      return;
+    }
+
     var status = await Permission.storage.status;
     if (!status.isGranted) {
       await Permission.storage.request();
